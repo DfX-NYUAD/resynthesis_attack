655362c73b98b5d8d18ab56335ab12f4ecb8735a
Johann Knechtel <jk176@b134-l-os22-ee.abudhabi.nyu.edu>
2023-04-04 16:53:10 +0400

    abc/convert.sh added semaphore handling, to protect common files during parallel processing in same work dir; abc/convert.sh added note on abc github repo

--- abc/convert.sh
+++ abc/convert.sh
@@ -8,6 +8,7 @@ pwd_="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 ## NOTE/TODO these parameters are to be revised to match the setup at your end
 # local (link to) abc binary
+# https://github.com/berkeley-abc/abc.git
 abc=$pwd_/abc
 # lib file
 lib=/home/jk176/old_home/work/Nangate/NangateOpenCellLibrary_typical.lib
@@ -24,36 +25,83 @@ verilog_in=design_in.v
 verilog_out=design_out.v
 lib_in=library.lib
 
+## procedures
+#####
+
+semaphore_enter() {
+	local mode=$1
+
+	semaphore=$mode'.semaphore'
+
+	while true; do
+
+		if [[ -e $semaphore ]]; then
+
+			sleep 1s
+		else
+			# check again after a little while; needed to mitigate race conditions where >1 processes just tried to enter the semaphore at the same time
+			# pick random value from 0.0 to 0.9 etc to 4.9, to hopefully avoid checking again at the very same time for >1 processes
+			sleep $(shuf -i 0-4 -n 1).$(shuf -i 0-9 -n 1)s
+
+			if ! [[ -e $semaphore ]]; then
+
+				# write/lock semaphore
+				date > $semaphore
+
+				return
+			fi
+		fi
+	done
+}
+
+semaphore_exit() {
+	local file=$1
+	local log=$2
+
+	semaphore=$mode'.semaphore'
+
+	# delete/release semaphore
+	# NOTE stderr occurs in case the semaphore was already deleted, meaning it was entered multiple times
+	rm $semaphore 2>> $log
+}
+
 ## check all parameters
 #####
-if [ $# -lt 2 ]; then
+if [ $# -lt 3 ]; then
 	echo "Parameters required:"
 	echo "1) Full path for input file to convert -- note that output file will be pushed to the same parent path/folder."
-	echo "2) Conversion option: b2v (bench to verilog), or v2b (verilog to bench)."
+	echo "2) Conversion option: 'b2v' (bench to verilog), or 'v2b' (verilog to bench)."
+	echo "3) Semaphore handling options: 'y' or 'n'. Use 'y' if you do parallel processing within the same work folder."
 	exit 1
 fi
 #
 file_in=$1
 if ! [[ -e $file_in ]]; then
-	echo "1) Input file does not exist; check the provided path: \"$file_in\"."
+	echo "Input file does not exist; check the provided path: \"$file_in\"."
 	exit 1
 elif [[ $file_in != *"/"* ]]; then
-	echo "1) Input file is lacking path; check the provided path: \"$file_in\"."
+	echo "Input file is lacking path; check the provided path: \"$file_in\"."
 	exit 1
 fi
 #
 mode=$2
 if ! [[ $mode == "b2v" || $mode == "v2b" ]]; then
-	echo "2) Unknown conversion option: \"$mode\" -- select either \"b2v\" or \"v2b\"."
+	echo "Unknown conversion option: \"$mode\" -- select either \"b2v\" or \"v2b\"."
+	exit 1
+fi
+#
+use_semaphore=$3
+if ! [[ $use_semaphore == "y" || $use_semaphore == "n" ]]; then
+	echo "Unknown semaphore handling option: \"$use_semaphore\" -- select either \"y\" or \"n\"."
 	exit 1
 fi
 #
 ## NOTE also sanity check on other parameters
 if ! [[ -e $abc ]]; then
-	echo "abc binary does not exist; check the provided path: \"$abc\"."
+	echo "'abc' binary does not exist; check the provided path: \"$abc\"."
 fi
 if ! [[ -e $lib ]]; then
-	echo "library files does not exist; check the provided path: \"$lib\"."
+	echo "Library files does not exist; check the provided path: \"$lib\"."
 fi
 
 ## derive runtime parameters
@@ -74,9 +122,16 @@ log_file=$file_in_path/$file_in_name'.'$mode'.log'
 #echo $log_file
 #exit
 
-## main code, based on mode
+## main code
 #####
+
+## main code, depending on mode
 if [[ $mode == "b2v" ]]; then
+
+	if [[ $use_semaphore == "y" ]]; then
+		# NOTE semaphore must be based on the generic $bench_in file, not the specific $file_in
+		semaphore_enter b2v
+	fi
 
 	ln -sf $lib $lib_in
 	ln -sf $file_in $bench_in
@@ -89,8 +144,16 @@ if [[ $mode == "b2v" ]]; then
 	unlink $lib_in
 	unlink $bench_in
 
+	if [[ $use_semaphore == "y" ]]; then
+		semaphore_exit b2v $log_file
+	fi
+
 # by construction, (i.e., check above) must be v2b
 else
+	if [[ $use_semaphore == "y" ]]; then
+		# NOTE semaphore must be based on the generic $verilog_in file, not the specific $file_in
+		semaphore_enter v2b
+	fi
 
 	ln -sf $lib $lib_in
 	ln -sf $file_in $verilog_in
@@ -102,4 +165,8 @@ else
 
 	unlink $lib_in
 	unlink $verilog_in
+
+	if [[ $use_semaphore == "y" ]]; then
+		semaphore_exit v2b $log_file
+	fi
 fi
